102b3a8c2e979e39ee553ab5bd4932e26d779301
padong2023 <128555332+padong2023@users.noreply.github.com>
2023-05-11 01:19:21 +0000

    switche문 추가

---
+++ mission2.c
@@ -0,0 +1,18 @@
+// //점수를 입력받아 90점 이상이면 A학점, 80이상이면 B학점, 70점 이상이면 C학점, 60점 이상이면 D학점, 60점 미만이면 F학점을 출력하는 프로그램을 작성
+// #include <stdio.h>
+// main(){
+//   int a;
+//   printf("나의 점수 입력: ");
+//   scanf("%d", &a);
+  
+//   if(a>=90)
+//     printf("A학점");
+//   else if(a>=80)
+//     printf("B학점");
+//   else if(a>=70)
+//     printf("C학점");
+//   else if(a>=60)
+//     printf("D학점");
+//   else
+//     printf("F학점");
+// }
